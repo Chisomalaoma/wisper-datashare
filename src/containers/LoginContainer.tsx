8daf32aafabb16e50e4908243c964d091ae54a5a
thexovc <osazeepeter79@gmail.com>
2025-06-28 10:09:49 +0100

    adding gradient to auth pages

--- src/containers/LoginContainer.tsx
+++ src/containers/LoginContainer.tsx
@@ -2,7 +2,7 @@ import LoginForm from '../components/forms/LoginForm';
 
 export default function LoginContainer() {
     return (
-        <div className="min-h-screen flex items-center justify-center">
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-400/40 to-purple-600/40">
             <div className="w-full max-w-md p-8 rounded-2xl shadow-xl bg-white/30 backdrop-blur-lg border border-white/30">
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
                 <LoginForm />
